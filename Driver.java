0fe1634f5d8b50d093578c93d1d064676ae3c4f6
Leo <proleo0518@gmail.com>
2025-09-28 23:14:18 -0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,25 @@
+import java.io.*;
+import java.util.*;
+
 public class Driver {
-public static void main(String [] args) {
-Polynomial p = new Polynomial();
-System.out.println(p.evaluate(3));
-double [] c1 = {6,0,0,5};
-Polynomial p1 = new Polynomial(c1);
-double [] c2 = {0,-2,0,0,-9};
-Polynomial p2 = new Polynomial(c2);
-Polynomial s = p1.add(p2);
-System.out.println("s(0.1) = " + s.evaluate(0.1));
-if(s.hasRoot(1))
-System.out.println("1 is a root of s");
-else
-System.out.println("1 is not a root of s");
-}
+    public static void main(String[] args) {
+        double[] coeff1 = {1, 6, 3};
+        int[] exp1 = {0, 1, 2};
+        Polynomial p1 = new Polynomial(coeff1, exp1);
+        
+        double[] coeff2 = {5, -8, 3};
+        int[] exp2 = {1, 2, 3};
+        Polynomial p2 = new Polynomial(coeff2, exp2);
+        
+        Polynomial result = p1.add(p2);
+        System.out.println("p1 + p2 result evaluated at x=2: " + result.evaluate(2));
+        
+        try {
+            p1.saveToFile("polynomial.txt");
+            Polynomial fromFile = new Polynomial(new File("polynomial.txt"));
+            System.out.println("File works");
+        } catch (IOException e) {
+            System.out.println("Something fucked up: " + e.getMessage());
+        }
+    }
 }
